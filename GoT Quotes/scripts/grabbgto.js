f37b8dd7785a23373914ed41fe039487c2581635
Josip Digital <info@digriot.com>
2017-01-28 19:29:37 +0100

    Add project files.

---
+++ GoT Quotes/scripts/grabbgto.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module('quote', [])
+app.controller('Hello', function ($scope, $http) {
+    $http.get('https://got-quotes.herokuapp.com/quotes').
+        then(function (response) {
+            $scope.quotes = response.data;
+        });
+});
